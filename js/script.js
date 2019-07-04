b612c37be24d12872153076ea034a8f2e8e1326b
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-07-04 23:24:38 +0200

    Fixed constructor, add result, fixed styles

--- js/script.js
+++ js/script.js
@@ -7,23 +7,38 @@ stopButton.addEventListener('click', () => stopwatch.stop());
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', () => stopwatch.reset());
 
+const timeButton = document.getElementById('time-capture');
+timeButton.addEventListener('click', () => stopwatch.result());
+
+
+const timeHistory = document.querySelector('.results');
+
 class Stopwatch {
+
   constructor(display) {
+    this.times = {
+        minutes: 0,
+        seconds: 0,
+        miliseconds: 0
+      };
     this.running = false;
     this.display = display;
-    this.reset();
     this.print(this.times);
   }
+
   start() {
     if (!this.running) {
       this.running = true;
       this.watch = setInterval(() => this.step(), 10);
     }
   }
+
   stop() {
     this.running = false;
     clearInterval(this.watch);
+    this.result();
   }
+
   reset() {
     this.times = {
       minutes: 0,
@@ -31,18 +46,31 @@ class Stopwatch {
       miliseconds: 0
     };
     this.print();
+    this.clearHistory();
   }
+
+  result() {
+    timeHistory.innerHTML += `<li class='time-result'>${this.format(this.times)}</li>`;
+  }
+
+  clearHistory() {
+    timeHistory.innerHTML = `<li class='time-result'>Your times:</li>`;
+  }
+
   step() {
     if (!this.running) return;
     this.calculate();
     this.print();
   }
+
   print() {
     this.display.innerText = this.format(this.times);
   }
+
   format(times) {
     return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
   }
+
   calculate() {
     this.times.miliseconds += 1;
     if (this.times.miliseconds >= 100) {
