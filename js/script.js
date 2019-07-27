ffddaff16671d6df65284f02406d3dc1ce9ec572
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-07-27 23:31:25 +0200

    Add time list - need fix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-class Stopwatch extends React.Component{
+class Stopwatch extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        minutes: 0,
-        seconds: 0,
-        miliseconds: 0,
-        running: false,
-        result: ''
-      };
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0,
+      running: false,
+      result: [],
+      time: 0
+    };
   }
 
   start() {
@@ -40,12 +41,21 @@ class Stopwatch extends React.Component{
   }
 
   result() {
+    const x = `${pad0(this.state.minutes)}:${pad0(this.state.seconds)}:${pad0(Math.floor(this.state.miliseconds))}`;
+    const times = [...this.state.result, TimesResult];
     this.setState({
-      result: this.format({
-        minutes: this.state.minutes,
-        seconds: this.state.seconds,
-        miliseconds: this.state.miliseconds
-      })
+      result: times,
+      time: x
+    })
+  }
+
+  timeList() {
+    let miliseconds = this.state.miliseconds;
+    let seconds = this.state.seconds;
+    let minutes = this.state.minutes;
+
+    this.setState({
+      time: miliseconds,seconds,minutes
     })
   }
 
@@ -71,28 +81,28 @@ class Stopwatch extends React.Component{
       miliseconds
     });
   }
-  
+
   render() {
     return (
       <main className="container">
         <section className="controls">
-          <a href="#" className="button" onClick={() => {this.start()}}> Start</a>
-          <a href="#" className="button" onClick={() => {this.stop()}}>Stop</a>
-          <a href="#" className="button" onClick={() => {this.reset()}}>Reset</a>
-          <a href="#" className="button" onClick={() => {this.result()}}>Time capture</a>
+          <a href="#" className="button" onClick={() => { this.start() }}> Start</a>
+          <a href="#" className="button" onClick={() => { this.stop() }}>Stop</a>
+          <a href="#" className="button" onClick={() => { this.reset() }}>Reset</a>
+          <a href="#" className="button" onClick={() => { this.result() }}>Time capture</a>
         </section>
-        < div className="stopwatch" > 
-        {this.format({minutes: this.state.minutes, seconds: this.state.seconds, miliseconds: this.state.miliseconds})} 
+        < div className="stopwatch" >
+          {this.format({ minutes: this.state.minutes, seconds: this.state.seconds, miliseconds: this.state.miliseconds })}
         </div>
         <ul className="results">
           <li className="time-result">Your times:</li>
-          <li className="time-result">{this.state.result}</li>
+          {this.state.result.map((TimesResult, index) => <TimesResult key={index} time={this.state.time} />)}
         </ul>
       </main>
     )
   }
 }
-
+const TimesResult = ({ time }) => <li className="time-result">{time}</li>
 const pad0 = value => {
   let result = value.toString();
   if (result.length < 2) {
